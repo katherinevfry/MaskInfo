6662d57338c8f9ad34ce7c943539fff709b13cb9
katherinevfry <katyvfry@gmail.com>
2021-07-18 18:32:38 -0500

    Add project files.

---
+++ MaskInfo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MaskInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tell me a secret");
+            var userInput = Console.ReadLine();
+
+            Console.WriteLine("Your secret was...");
+            string secret;
+            if (userInput.Length > 4)
+            {
+              secret = userInput.Substring(userInput.Length - 4).PadLeft(userInput.Length, '*');
+              
+            }
+            else
+            {
+                secret = "That's not a very long secret.";
+            }
+            Console.WriteLine(secret);
+        }
+    }
+}
